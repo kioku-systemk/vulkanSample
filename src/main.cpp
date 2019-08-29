6ccdd927ecb9b449c07ca05be7656ad334794ff6
kioku <gkioku@gmail.com>
2019-08-29 20:21:12 +0900

    [WIP] update

--- src/main.cpp
+++ src/main.cpp
@@ -15,18 +15,32 @@ VkSwapchainKHR _swapchain = VK_NULL_HANDLE;
 std::vector<const char*> instance_layers;
 std::vector<const char*> instance_extensions;
 
-VkInstance initInstance()
+void dumpExtensions() {
+	// Extensions
+	uint32_t extensionCount = 0;
+	vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount, nullptr);
+	std::cout << extensionCount << " extensions supported" << std::endl;
+	std::vector<VkExtensionProperties> extensions(extensionCount);
+	vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount, extensions.data());
+	for (const auto& extension : extensions) {
+		std::cout << "\t" << extension.extensionName << std::endl;
+	}
+}
+
+VkInstance createInstance(const char* appName)
 {
 	VkInstance instance = nullptr;
-	instance_layers.push_back("VK_LAYER_LUNARG_standard_validation");
+	instance_layers.push_back("VK_LAYER_LUNARG_standard_validation"); // for Debug
 	instance_extensions.push_back(VK_KHR_SURFACE_EXTENSION_NAME);
-	instance_extensions.push_back(VK_KHR_WIN32_SURFACE_EXTENSION_NAME);
-	
+#ifdef _WIN32
+	instance_extensions.push_back(VK_KHR_WIN32_SURFACE_EXTENSION_NAME); // need for Windows
+#endif
+
 	VkApplicationInfo application_info{};
 	application_info.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
 	application_info.apiVersion = VK_MAKE_VERSION(1, 0, 3);
 	application_info.applicationVersion = VK_MAKE_VERSION(0, 1, 0);
-	application_info.pApplicationName = "MyVulkanApp";
+	application_info.pApplicationName = appName;
 
 	VkInstanceCreateInfo instance_create_info{};
 	instance_create_info.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
@@ -47,23 +61,123 @@ VkInstance initInstance()
 	return instance;
 }
 
-void vulkanInit() {
-	// Extensions
-	uint32_t extensionCount = 0;
-	vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount, nullptr);
-	std::cout << extensionCount << " extensions supported" << std::endl;
-	std::vector<VkExtensionProperties> extensions(extensionCount);
-	vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount, extensions.data());
-	for (const auto& extension : extensions) {
-		std::cout << "\t" << extension.extensionName << std::endl;
+bool isGPUCheck(VkPhysicalDevice device) {
+	VkPhysicalDeviceProperties deviceProperties;
+	VkPhysicalDeviceFeatures deviceFeatures;
+	vkGetPhysicalDeviceProperties(device, &deviceProperties);
+	vkGetPhysicalDeviceFeatures(device, &deviceFeatures);
+	return deviceProperties.deviceType == VK_PHYSICAL_DEVICE_TYPE_DISCRETE_GPU &&
+		deviceFeatures.geometryShader;
+}
+
+VkPhysicalDevice createDevice(VkInstance instance)
+{
+	VkPhysicalDevice physicalDevice = VK_NULL_HANDLE;
+
+	uint32_t deviceCount = 0;
+	vkEnumeratePhysicalDevices(instance, &deviceCount, nullptr);
+	if (deviceCount == 0) {
+		assert(0 && "failed to find GPUs with Vulkan support!");
+		std::exit(-1);
+		return nullptr;
+	}
+	std::vector<VkPhysicalDevice> devices(deviceCount);
+	vkEnumeratePhysicalDevices(instance, &deviceCount, devices.data());
+
+	for (const auto& device : devices) {
+		if (isGPUCheck(device)) {
+			physicalDevice = device;
+			break;
+		}
+	}
+	if (physicalDevice == VK_NULL_HANDLE) {
+		assert(0 && "failed to find a suitable GPU!");
 	}
 
+	return physicalDevice;
+}
+
+class QueueFamilyIndices {
+public:
+	uint32_t graphicsFamily;
+};
+
+QueueFamilyIndices findQueueFamilies(VkPhysicalDevice device) {
+	QueueFamilyIndices indices;
+
+	uint32_t queueFamilyCount = 0;
+	vkGetPhysicalDeviceQueueFamilyProperties(device, &queueFamilyCount, nullptr);
+
+	std::vector<VkQueueFamilyProperties> queueFamilies(queueFamilyCount);
+	vkGetPhysicalDeviceQueueFamilyProperties(device, &queueFamilyCount, queueFamilies.data());
+
+	int i = 0;
+	for (const auto& queueFamily : queueFamilies) {
+		if (queueFamily.queueCount > 0 && queueFamily.queueFlags & VK_QUEUE_GRAPHICS_BIT) {
+			indices.graphicsFamily = i;
+		}
+
+		i++;
+	}
+	return indices;
+}
+
+VkDevice createLogicalDevice(VkPhysicalDevice physicalDevice)
+{
+	VkDevice device = nullptr;
+	QueueFamilyIndices indices = findQueueFamilies(physicalDevice);
+
+	VkDeviceQueueCreateInfo queueCreateInfo = {};
+	queueCreateInfo.sType = VK_STRUCTURE_TYPE_DEVICE_QUEUE_CREATE_INFO;
+	queueCreateInfo.queueFamilyIndex = indices.graphicsFamily;
+	queueCreateInfo.queueCount = 1;
+	float queuePriority = 1.0f;
+	queueCreateInfo.pQueuePriorities = &queuePriority;
+
+	VkPhysicalDeviceFeatures deviceFeatures = {};
+	VkDeviceCreateInfo createInfo = {};
+	createInfo.sType = VK_STRUCTURE_TYPE_DEVICE_CREATE_INFO;
+	createInfo.pQueueCreateInfos = &queueCreateInfo;
+	createInfo.queueCreateInfoCount = 1;
+	createInfo.pEnabledFeatures = &deviceFeatures;
+	createInfo.enabledExtensionCount = 0;
+	createInfo.enabledLayerCount = 0;
+
+	if (vkCreateDevice(physicalDevice, &createInfo, nullptr, &device) != VK_SUCCESS) {
+		throw std::runtime_error("failed to create logical device!");
+	}
+
+	return device;
+}
+
+void vulkanInit(GLFWwindow* window) {
+	
+	dumpExtensions();
+
 	// create Instance
-	VkInstance	vkInst = initInstance();
+	VkInstance	vkInst = createInstance("MyApp");
+	VkSurfaceKHR surface;
+
+	// create Surface
+	VkResult err = glfwCreateWindowSurface(vkInst, window, NULL, &surface);
+	if (err) {
+		assert(0 && "Vulkan ERROR: Create WindowSurface failed!!");
+		std::exit(-1);
+		return;
+	}
 
 	// create Device
+	VkPhysicalDevice physicalDevice = createDevice(vkInst);
+
+	// create LogicalDevice
+	VkDevice dev = createLogicalDevice(physicalDevice);
 
 	// ...
+}
+
+void vulkanCleanup(VkInstance instance, VkSurfaceKHR surface) {
+	vkDestroySurfaceKHR(instance, surface, nullptr);
+	vkDestroyInstance(instance, nullptr);
 }
 
 int main() {
@@ -72,7 +186,7 @@ int main() {
 	glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
     window = glfwCreateWindow(300,400, "vukan tutorial",nullptr,nullptr );
 
-	vulkanInit();
+	vulkanInit(window);
 	
     while (!glfwWindowShouldClose(window));
     {
